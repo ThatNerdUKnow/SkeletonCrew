b5777d43afee70e6cee97fa7ebacde75e7ab2728
Brandon Piña <brandonpi265@gmail.com>
2023-07-29 11:05:37 -0500

    sprite facing

--- player.gd
+++ player.gd
@@ -5,6 +5,9 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var _animation = $AnimationPlayer
+@onready var _sprite = $Sprite2D
+
+var facing_direction = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -23,7 +26,12 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up","ui_down")
+	
+	if direction_x != 0:
+		facing_direction = direction_x
+	
 	if direction_x:
+		_animation.play("Walk")
 		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -32,10 +40,15 @@ func _physics_process(delta):
 	else:
 		velocity.y = move_toward(velocity.y,0,SPEED)
 
+	sprite_face_direction()
 	move_and_slide()
 
 
-
+func sprite_face_direction():
+	if facing_direction < 0:
+		_sprite.flip_h = true
+	else:
+		_sprite.flip_h = false
 
 
 func _on_area_2d_body_entered(body):
