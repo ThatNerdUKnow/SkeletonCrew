fa9d1dee661c3e697f2f291c7005efdb71bf0040
Brandon Piña <brandonpi265@gmail.com>
2023-07-30 10:45:16 -0500

    fix controller support

--- EndGame/Button.gd
+++ EndGame/Button.gd
@@ -10,6 +10,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		_on_pressed()
 	pass
 
 
